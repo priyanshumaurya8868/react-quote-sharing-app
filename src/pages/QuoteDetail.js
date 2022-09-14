a2635ed90955e13fd73012327d57a1bda8d07bc6
Priyanshu <priyanshumaurya.8868@gmail.com>
2022-09-14 20:20:05 +0530

    queryParams + nesting routes

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import { Route, useParams } from "react-router-dom";
+import { Route, useHistory, useParams } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import NoQuotesFound from "../components/quotes/NoQuotesFound";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
@@ -8,20 +8,32 @@ const DUMMY_QUOTES = [
   { id: "q2", author: "Maximilian", text: "Learning React is great!" },
 ];
 export default function QuoteDetail() {
+  const history = useHistory();
   const quoteId = useParams().quoteId;
   console.log(quoteId);
 
   const quote = DUMMY_QUOTES.find((item) => item.id === quoteId);
 
   if (!quote) {
-    return <NoQuotesFound/>;
+    return <NoQuotesFound />;
   }
 
   console.log(quote);
 
+  const viewCommentsHandler = () => {
+    history.replace(`/quotes/${quoteId}/comments`);
+  };
+
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${quoteId}`} exact>
+        <div className="centered">
+          <button onClick={viewCommentsHandler} className="btn--flat">
+            View Comments
+          </button>
+        </div>
+      </Route>
       <Route path={`/quotes/${quoteId}/comments`}>
         <Comments />
       </Route>
