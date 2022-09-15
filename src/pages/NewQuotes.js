35018f8d60ab8445722c36c47feaf4897c992f1d
Priyanshu <priyanshumaurya.8868@gmail.com>
2022-09-15 05:31:01 +0530

    firebase

--- src/pages/NewQuotes.js
+++ src/pages/NewQuotes.js
@@ -1,11 +1,27 @@
-import React from "react";
+import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
+
 import QuoteForm from "../components/quotes/QuoteForm";
-export default function NewQuotes() {
-    const history = useHistory()
-  const addQuoteHandler = (quote) => {
-    console.log(quote);
-    history.replace('/quotes')
+import useHttp from "../hooks/use-http";
+import { addQuote } from "../lib/api";
+
+const NewQuote = () => {
+  const { sendRequest, status } = useHttp(addQuote);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (status === "completed") {
+      history.push("/quotes");
+    }
+  }, [status, history]);
+
+  const addQuoteHandler = (quoteData) => {
+    sendRequest(quoteData);
   };
-  return <QuoteForm onAddQuote={addQuoteHandler} />;
-}
+
+  return (
+    <QuoteForm isLoading={status === "pending"} onAddQuote={addQuoteHandler} />
+  );
+};
+
+export default NewQuote;
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,40 +1,57 @@
-import React, { Fragment } from "react";
-import { Route, useHistory, useParams } from "react-router-dom";
+import React, { Fragment, useEffect } from "react";
+import {
+  Link,
+  Route,
+  useParams,
+  useRouteMatch,
+} from "react-router-dom";
 import Comments from "../components/comments/Comments";
-import NoQuotesFound from "../components/quotes/NoQuotesFound";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
-const DUMMY_QUOTES = [
-  { id: "q1", author: "Max", text: "Learning React is fun!" },
-  { id: "q2", author: "Maximilian", text: "Learning React is great!" },
-];
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+
 export default function QuoteDetail() {
-  const history = useHistory();
+  const match = useRouteMatch();
   const quoteId = useParams().quoteId;
-  console.log(quoteId);
 
-  const quote = DUMMY_QUOTES.find((item) => item.id === quoteId);
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true);
 
-  if (!quote) {
-    return <NoQuotesFound />;
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+  if (error) {
+    return <p className="centered">{error}</p>;
   }
 
-  console.log(quote);
-
-  const viewCommentsHandler = () => {
-    history.replace(`/quotes/${quoteId}/comments`);
-  };
+  if (!loadedQuote.text) {
+    return <p>No quote found!</p>;
+  }
 
   return (
     <Fragment>
-      <HighlightedQuote text={quote.text} author={quote.author} />
-      <Route path={`/quotes/${quoteId}`} exact>
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
+      <Route path={match.path} exact>
         <div className="centered">
-          <button onClick={viewCommentsHandler} className="btn--flat">
-            View Comments
-          </button>
+          <Link className="btn--flat" to={`${match.url}/comments`}>
+            Load Comments
+          </Link>
         </div>
       </Route>
-      <Route path={`/quotes/${quoteId}/comments`}>
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
     </Fragment>
